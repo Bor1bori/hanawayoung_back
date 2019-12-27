25d58c81db4126b1b4e651e3b278c6b85bfef9a8
bor1bori <bor1boridv@gmail.com>
2019-12-28 02:06:37 +0900

    expo sendPush service 한번에 보내는걸로 변경

--- src/routes/help.ts
+++ src/routes/help.ts
@@ -2,16 +2,18 @@ import Express from 'express';
 import * as authMiddleware from '../middleware/auth';
 import * as helpController from '../controller/help';
 import {UserModel} from '../model/user';
-import {sendPush} from '../services/expo';
+import {sendPushs} from '../services/expo';
 const Router = Express.Router();
 
 Router.post('/', authMiddleware.verifyLogin, helpController.postHelp);
 Router.get('/test', helpController.postHelp, async (req, res) => {
   try {
     const users = await UserModel.find({token: {$exists: true}});
+    const pushTokens = [];
     for (let i = 0 ; i < users.length ; i++) {
-      await sendPush('testeset테스트중asdf', users[i].token.value);
+      pushTokens.push(users[i].token.value);
     }
+    await sendPushs('testeset테스트중asdf', pushTokens);
   } catch(err){
     console.log(err);
   }
--- src/services/expo.ts
+++ src/services/expo.ts
@@ -2,20 +2,43 @@ import Expo from 'expo-server-sdk';
 
 const expo = new Expo();
 
-export const sendPush = async (message: string, pushToken: string) => {
-  if (!Expo.isExpoPushToken(pushToken)) {
-    console.error(`Push token ${pushToken} is not a valid Expo push token`);
-    return null;
+export const sendPushs = async (message: string, pushTokens: any[]) => {
+  const messages: any = [];
+  for (const pushToken of pushTokens) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications)
+    messages.push({
+      to: pushToken,
+      sound: 'default',
+      body: message,
+      data: { withSome: 'data' },
+    });
   }
-  const notification: any = {
-    to: pushToken,
-    sound: 'default',
-    title: 'Message received!',
-    body: message,
-    data: { message }
-  };
-  const chunks = expo.chunkPushNotifications([notification]);
-  const receipts = await expo.sendPushNotificationsAsync(chunks[0]);
-  console.log(receipts);
-  return receipts;
+  const chunks = expo.chunkPushNotifications(messages);
+  const tickets = [];
+  await (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (const chunk of chunks) {
+      try {
+        const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
 };
